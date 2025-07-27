d9908a43d2ca8604e2c07216dcecaa8c6bf9a30e
LPastore25 <lsp54@case.edu>
2025-07-27 14:37:04 -0400

    Turned on production

--- online-web-ui/backend/app.py
+++ online-web-ui/backend/app.py
@@ -6,7 +6,7 @@ from flask import send_from_directory
 
 app = Flask(__name__, static_folder="../client/build", static_url_path="")
 CORS(app)
-PRODUCTION_MODE = False 
+PRODUCTION_MODE = True 
 DB_FILE = os.path.join(os.path.dirname(__file__), "acs_data.db")
 
 
